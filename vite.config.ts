12e727724c1988f0fa68498e40c335ac0716fa3a
Waaatanuki <watanukipublic@gmail.com>
2024-08-07 11:21:28 +0800

    Init

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import Vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
-import VueMacros from 'unplugin-vue-macros/vite'
 import VueRouter from 'unplugin-vue-router/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 
@@ -15,23 +14,8 @@ export default defineConfig({
     },
   },
   plugins: [
-    VueMacros({
-      defineOptions: false,
-      defineModels: false,
-      plugins: {
-        vue: Vue({
-          script: {
-            propsDestructure: true,
-            defineModel: true,
-          },
-        }),
-      },
-    }),
-
-    // https://github.com/posva/unplugin-vue-router
+    Vue(),
     VueRouter(),
-
-    // https://github.com/antfu/unplugin-auto-import
     AutoImport({
       imports: [
         'vue',
@@ -48,14 +32,9 @@ export default defineConfig({
       ],
       vueTemplate: true,
     }),
-
-    // https://github.com/antfu/vite-plugin-components
     Components({
       dts: true,
     }),
-
-    // https://github.com/antfu/unocss
-    // see uno.config.ts for config
     UnoCSS(),
   ],
 })
